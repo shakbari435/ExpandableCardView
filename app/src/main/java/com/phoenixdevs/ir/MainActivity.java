d2d6fed4f430dd86a08b2f843734c85321f823b4
shakbari435 <mylove313_sha@yahoo.com>
2018-07-26 16:28:01 +0430

    Initial commit

---
+++ app/src/main/java/com/phoenixdevs/ir/MainActivity.java
@@ -0,0 +1,37 @@
+package com.phoenixdevs.ir;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        final ExpandableCardView expandableCardView=findViewById(R.id.ecv2);
+
+        expandableCardView.setRtlDirection();
+        expandableCardView.setTitle("Profile");
+        expandableCardView.setIcon(R.drawable.ic_user);
+
+        expandableCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(expandableCardView.isExpanded()){
+                    expandableCardView.collapse();
+                    Toast.makeText(MainActivity.this, "Close", Toast.LENGTH_SHORT).show();
+
+                }
+                else{
+                    expandableCardView.expand();
+                    Toast.makeText(MainActivity.this, "Open", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
